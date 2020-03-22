71bf863717318d98eae2fee7f8630ce5f9df9a28
okapie <tatigreuze@yahoo.co.jp>
2020-03-22 10:58:58 +0900

    Create Form.vue.

---
+++ src/components/Form.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <label>{{ label }}: <input type="text" v-model="todo"></label>
+    <p>{{ todo }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Form extends Vue {
+  @Prop() private label!: string;
+  data () {
+    return {
+      todo: ''
+    }
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <Form label="Input new item" />
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,11 +9,13 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Form from '@/components/Form.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    Form
   }
 }
 </script>
