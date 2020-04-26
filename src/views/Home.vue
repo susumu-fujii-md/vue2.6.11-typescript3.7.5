befca190e59ae40b0d2d0546f25d636399ab46e2
okapie <tatigreuze@yahoo.co.jp>
2020-04-26 09:31:01 +0900

    Delete HelloWorld.vue.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,19 +2,16 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <Form label="Input new item" />
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import Form from '@/components/Form.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
     Form
   }
 }
